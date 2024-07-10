17054cdd84cc64d74a309dbf8f690886be0b514d
woojin <woojinchoi1023@gmail.com>
2024-07-10 17:57:27 +0900

    0710

--- 0710수/LCS4.py
+++ 0710수/LCS4.py
@@ -1,9 +1,11 @@
 # https://www.acmicpc.net/problem/13711
+
+import bisect
+
 N = int(input())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-# ls = [[0]*(N+1) for _ in range(N+1)]
 idx = {}
 for i in range(N):
     idx.setdefault(a[i], [])
@@ -12,37 +14,28 @@ for i in range(N):
 arr = []
 for i in range(N):
     if idx.get(b[i], []):
-        max(arr)
+        temp = idx[b[i]].pop(0)
+        pos = bisect.bisect_left(arr, temp)
+        if len(arr) > pos:
+            arr[pos] = temp
+        else:
+            arr.append(temp)
+    # print(arr)
 
 
-print(idx)
+print(len(arr))
 
 
 '''
 7
 1 1 1 1 2 3 4
 1 2 3 4 1 1 1
-
-{1: [0, 1, 2, 3], 2: [4], 3: [5], 4: [6]}
-arr
-0 4 5 6
-0 1 5 6
-0 1 2 6
-0 1 2 3
-
-  0 1 1 1 1 2 3 4
-0 0 0 0 0 0 0 0 0
-1 0 1 1 1 1 1 1 1
-2 0 1 1 1 1 2 2 2
-3 0 1 1 1 1 2 3 3
-4 0
-1 0
-1 0
-1 0
-
--> 1, 3, 4
--> 1, 2, 4
-
-1 2
-
+[0]
+[0, 4]
+[0, 4, 5]
+[0, 4, 5, 6]
+[0, 1, 5, 6]
+[0, 1, 2, 6]
+[0, 1, 2, 3]
+4
 '''
