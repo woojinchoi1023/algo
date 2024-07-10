27acac71460f1464baa4daa144704f5cabc68a0e
woojin <woojinchoi1023@gmail.com>
2024-07-10 17:47:52 +0900

    0710

--- 0710수/LCS4.py
+++ 0710수/LCS4.py
@@ -3,18 +3,33 @@ N = int(input())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-ls = [[0]*(N+1) for _ in range(N+1)]
+# ls = [[0]*(N+1) for _ in range(N+1)]
+idx = {}
+for i in range(N):
+    idx.setdefault(a[i], [])
+    idx[a[i]].append(i)
 
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        if a[i-1] == b[j-1]:
-            ls[i][j] = ls[i-1][j-1]+1
-        else:
-            ls[i][j] = max(ls[i-1][j], ls[i][j-1])
-print(*ls, sep='\n')
+arr = []
+for i in range(N):
+    if idx.get(b[i], []):
+        max(arr)
+
+
+print(idx)
 
 
 '''
+7
+1 1 1 1 2 3 4
+1 2 3 4 1 1 1
+
+{1: [0, 1, 2, 3], 2: [4], 3: [5], 4: [6]}
+arr
+0 4 5 6
+0 1 5 6
+0 1 2 6
+0 1 2 3
+
   0 1 1 1 1 2 3 4
 0 0 0 0 0 0 0 0 0
 1 0 1 1 1 1 1 1 1
